537ed7d179b03763b5844ff67051b92c25680068
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-09-09 03:20:24 -0500

    sip

--- src/app/context/MovieContext.tsx
+++ src/app/context/MovieContext.tsx
@@ -1,22 +1,26 @@
-'use client'
-import { createContext, useState } from "react";
-
-
+import React, { createContext, useContext, useState, Dispatch, SetStateAction } from 'react';
 
 interface MovieContextType {
     url: string;
-    setUrl: () => void;
+    setUrl: Dispatch<SetStateAction<string>>;
 }
 
-export const MovieContext = createContext<MovieContextType | undefined>(undefined);
+const MovieContext = createContext<MovieContextType | undefined>(undefined);
 
-export const MovieProvider = ({ children }: { children: React.ReactNode }) => {
-
-    const [url, setUrl] = useState<string>('https://api.themoviedb.org/3/movie/popular?language=en-US&page=');
+export const MovieProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [url, setUrl] = useState<string>('');
 
     return (
         <MovieContext.Provider value={{ url, setUrl }}>
             {children}
         </MovieContext.Provider>
-    )
-}
+    );
+};
+
+export const useMovieContext = () => {
+    const context = useContext(MovieContext);
+    if (!context) {
+        throw new Error('useMovieContext debe ser usado dentro de MovieProvider');
+    }
+    return context;
+};
