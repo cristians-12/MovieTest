bb8bbaed0bf51a5b9c61130b5b842f8062180538
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-09-09 03:13:20 -0500

    popup register

--- src/app/components/RegisterModal.tsx
+++ src/app/components/RegisterModal.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const RegisterModal = () => {
   return (
-    <div>
-        <div className='fixed top-20 right-0'>
-            <h1>Hola</h1>
+    <div className='fixed flex z-50 w-screen items-center h-screen'>
+        <div className='fixed flex items-center top-52 right-40 backdrop-blur-sm w-[55vw] rounded-3xl h-[50vh] border border-white'>
+            <div className='flex'>
+                <div className='px-5'>
+                    Sign Up
+                </div>
+                <div>
+                    Login 
+                </div>
+            </div>
+            <div className='absolute bg-black right-0 w-[50%] top-0 rounded-tr-3xl rounded-br-3xl h-full'>
+                <p className='text-[40px] text-center font-bold mt-10'>Welcome to Quickbet Movies! </p>
+                <p className='text-center mt-20'>Ready to unlock a universe of cinematic delights?</p>
+            </div>
         </div>
     </div>
   )
