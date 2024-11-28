8f62a0263ef552c6f6da662ade9666b398931dd7
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-11-27 22:10:19 -0500

    fixed moviecard

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,14 +2,12 @@
 import React from "react";
 import useFetch from "./hooks/useFetch";
 import MainLayout from "./layouts/MainLayout";
-import { useMovieContext } from "./context/MovieContext";
 import Paginator from "@/components/Paginator";
 import MovieList from "@/components/MovieList";
 import useAppStore from "@/store/appStore";
 
 export default function Home() {
-  const { page, url } = useAppStore();
-  const { tag } = useMovieContext();
+  const { page, url, genre } = useAppStore();
   const { data } = useFetch(`${url}&page=${page}`);
 
   if (!data) {
@@ -20,7 +18,7 @@ export default function Home() {
     <>
       <MainLayout>
         <Paginator pages={data.total_pages} />
-        <h1 className="text-[40px] my-10">{tag}</h1>
+        <h1 className="text-[40px] my-10">Resultados para {genre.tag}</h1>
         <MovieList data={data} />
         <Paginator pages={data.total_pages} />
       </MainLayout>
