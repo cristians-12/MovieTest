2a1b9163e26f8f43351fc388dd7d4257a01ca7de
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-11-27 22:38:59 -0500

    yupilian

--- frontend/src/components/MovieList.tsx
+++ frontend/src/components/MovieList.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import { DataFetch } from '../types';
-import MovieCard from './includes/MovieCard';
+import React from "react";
+import MovieCard from "./includes/MovieCard";
+import { DataFetch, Movie } from "@/types/movie/movie.type";
 
 export default function MovieList({ data }: { data: DataFetch }) {
-    return (
-        <main>
-            <div className='flex flex-wrap gap-10 justify-center lg:overflow-x-auto lg:py-16'>
-                {data.results.length > 1 ? data.results.map(e => (
-                    <MovieCard key={e.id} e={e} />
-                )) : (
-                    data.results.length === 1 && <MovieCard e={data.results[0]} />
-                )}
-            </div>
-        </main>
-    )
+  return (
+    <main>
+      <div className="flex flex-wrap gap-10 justify-center lg:overflow-x-auto lg:py-16">
+        {data.results.length > 1
+          ? data.results.map((e: Movie) => <MovieCard key={e.id} e={e} />)
+          : data.results.length === 1 && <MovieCard e={data.results[0]} />}
+      </div>
+    </main>
+  );
 }
